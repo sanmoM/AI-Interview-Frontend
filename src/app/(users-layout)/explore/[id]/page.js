f355e6e2ca0a70436316a063626ec8650a0d3f97
anmobd111 <sanmobd111@gmail.com>
2026-04-21 17:26:29 +0600

    fix

--- src/app/(users-layout)/explore/[id]/page.js
+++ src/app/(users-layout)/explore/[id]/page.js
@@ -1,12 +1,10 @@
 "use client";
 
 import JobApplication from "@/components/single-job/job-application/job-application";
-import SingleJobSidebar from "@/components/single-job/single-job-sidebar";
 
 export default function page() {
   return (
     <div className="h-full flex flex-col lg:flex-row gap-6 xl:gap-8 w-full">
-      {/* <SingleJobSidebar /> */}
       <JobApplication />
     </div>
   );
