b41756f3667d0ee9b35747065b49aff6e51e35ea
anmobd111 <sanmobd111@gmail.com>
2026-04-21 17:58:31 +0600

    ok

--- src/app/(users-layout)/page.js
+++ src/app/(users-layout)/page.js
@@ -1,11 +1,19 @@
-"use client";
+// "use client";
 
-import JobApplication from "@/components/single-job/job-application/job-application";
+// import JobApplication from "@/components/single-job/job-application/job-application";
+
+// export default function page() {
+//   return (
+//     <div className="h-full flex flex-col lg:flex-row gap-6 xl:gap-8 w-full">
+//       <JobApplication />
+//     </div>
+//   );
+// }
+
+import React from 'react'
 
 export default function page() {
   return (
-    <div className="h-full flex flex-col lg:flex-row gap-6 xl:gap-8 w-full">
-      <JobApplication />
-    </div>
-  );
+    <div>page</div>
+  )
 }
